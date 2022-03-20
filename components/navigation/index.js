a7bd44643986b313e20db9a1cd773266d43b2bef
MacwithaQ <maqallaf@gmail.com>
2022-03-20 20:17:16 +0300

    add register page

--- components/navigation/index.js
+++ components/navigation/index.js
@@ -2,13 +2,18 @@ import { Pressable, StyleSheet } from "react-native";
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "../Home";
-
+import Register from "../Register";
 const { Navigator, Screen } = createNativeStackNavigator();
 
 const RootNavigator = () => {
   return (
-    <Navigator initialRouteName="Home">
+    <Navigator initialRouteName="Register">
       <Screen name="Home" component={Home} options={{ headerShown: false }} />
+      <Screen
+        name="Register"
+        component={Register}
+        options={{ headerShown: false }}
+      />
     </Navigator>
   );
 };
