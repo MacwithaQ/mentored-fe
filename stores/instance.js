37072f19a4898d9dccb1d5315f805c2b7163a5cf
abdullah-alkhareji <outsiderandco@gmail.com>
2022-03-22 18:20:49 +0300

    fix profile problem

--- stores/instance.js
+++ stores/instance.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 export const baseURL = "http://localhost:8000";
 
+//?Khareji's at Coded
+// export const baseURL = "http://192.168.100.77:8000";
+
 export const instance = axios.create({
   baseURL: `${baseURL}/api`,
 });
