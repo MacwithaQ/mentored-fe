f53009eddd6e9f5a9c520e554aa7d6ac949985cc
abdullah-alkhareji <outsiderandco@gmail.com>
2022-03-24 12:17:19 +0300

    refactor the input

--- components/Input.js
+++ components/Input.js
@@ -1,36 +1,18 @@
 import { StyleSheet, TextInput } from "react-native";
 import React from "react";
 
-const Input = ({
-  placeholder,
-  value,
-  onChangeText,
-  keyboardType,
-  secureTextEntry,
-  defaultValue,
-}) => {
-  return (
-    <TextInput
-      autoCapitalize="none"
-      autoComplete="off"
-      autoCorrect={false}
-      placeholder={placeholder}
-      value={value}
-      defaultValue={defaultValue}
-      secureTextEntry={secureTextEntry}
-      keyboardType={keyboardType}
-      onChangeText={onChangeText}
-      style={{
-        backgroundColor: "#F5F4F9",
-        paddingVertical: 10,
-        paddingHorizontal: 15,
-        marginVertical: 5,
-        borderRadius: 100,
-      }}
-    />
-  );
+const Input = ({ ...props }) => {
+  return <TextInput {...props} style={styles.input} />;
 };
 
 export default Input;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    backgroundColor: "#F5F4F9",
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    marginVertical: 5,
+    borderRadius: 100,
+  },
+});
